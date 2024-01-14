dcbb332e49a9900300304ccd95fab864b27c01e2
Emmanuel Odii <emmanuelodii80@gmail.com>
2024-01-14 21:53:37 +0100

    feat: add padding to contents

--- src/components/wrapper.tsx
+++ src/components/wrapper.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export function Wrapper(props: React.PropsWithChildren) {
   return (
-    <main className="min-h-screen w-full max-w-4xl flex flex-col gap-4 py-6 lg:py-10 mx-auto">
+    <main className="min-h-screen w-full max-w-4xl flex flex-col gap-4 p-6 lg:px-0 lg:py-10 mx-auto">
       {props.children}
     </main>
   );
