0ed3975f6043e5a8466e92df568c06be8b865647
devodii <emmanuelodii80@gmail.com>
2024-05-05 20:42:30 +0100

    chore: add back buton

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getArticles } from "@/action/articles";
+import { Back } from "@/components/back";
 
 interface Props {
   params: {
@@ -15,7 +16,10 @@ export default async function BlogViewPage({ params }: Props) {
 
   return (
     <section className="my-24 space-y-6">
-      <h3 className="font-medium text-gray-100 text-2xl">{article.title}</h3>
+      <div className="flex items-center gap-3">
+        <Back />
+        <h3 className="font-medium text-gray-100 text-2xl">{article.title}</h3>
+      </div>
       <article
         className="space-y-2 [&>_figure>_img]:rounded-md [&>_figure>_img]:mx-auto [&>_figure>_figcaption]:text-center [&>_figure]:py-6 [&>_a]:underline [&>_a>_em]:underline [&>_h4]:text-xl  [&>_h4]:font-semibold"
         dangerouslySetInnerHTML={{ __html: article.content }}
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -4,14 +4,16 @@ import * as React from "react";
 export default async function BlogsPage() {
   return (
     <section className="my-12 space-y-6">
-      <h2 className="text-2xl lg:text-4xl font-bold text-center">
+      <h2 className="text-2xl lg:text-4xl mt-4 font-bold text-center">
         ⚡️ Some stuff that i wrote about.
       </h2>
 
       <React.Suspense
         fallback={<div className="text-center mt-12">Loading articles...</div>}
       >
-        <Blogs />
+        <div className="mt-12">
+          <Blogs />
+        </div>
       </React.Suspense>
     </section>
   );
